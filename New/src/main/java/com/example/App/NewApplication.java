2421fd578808cf7e328e0059ba27176f82ba471b
ShivamWadkar <79800623+ShivamWadkar@users.noreply.github.com>
2022-04-28 21:19:29 +0530

    commited

---
+++ New/src/main/java/com/example/App/NewApplication.java
@@ -0,0 +1,15 @@
+package com.example.App;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan(basePackages = "com.example")
+public class NewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewApplication.class, args);
+	}
+
+}
